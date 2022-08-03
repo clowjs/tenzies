9cbc7904273606dbbcd74eb072c71c877b9b4c72
Sergio Jose dos Santos Junior <hagare@gmail.com>
2022-08-03 14:03:13 -0300

    Added Roll button

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,19 @@ import './index.css'
 function App() {
 
   const [dice, setDice] = useState(allNewDice())
+  
     
-    function allNewDice() {
-        const newDice = []
-        for (let i = 0; i < 10; i++) {
-            newDice.push(Math.ceil(Math.random() * 6))
-        }
-        return newDice
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+        newDice.push(Math.ceil(Math.random() * 6))
     }
+    return newDice
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
     
     const diceElements = dice.map(die => (
         <Die value={die} key={Math.random()} />
@@ -22,6 +27,7 @@ function App() {
             <div className="dice-container">
                 {diceElements}
             </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
         </main>
     )
 }
